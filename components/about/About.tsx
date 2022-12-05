e052bd6e37e272e2fc6a929cbec641526fa7c10d
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-05 00:23:47 -0300

    About section

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,9 +1,28 @@
 import AboutST from '../../styles/About.module.css'
+import Image from 'next/image'
 import React from 'react'
+import aboutImage from '../../public/assets/about_image.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className={`container ${AboutST.container}`}>
+      <div className={AboutST.image}>
+        <Image src={aboutImage} alt="About us image"/>
+      </div>
+      <div className={AboutST.text_container}>
+        <div className={AboutST.inner_text_container}>
+          <h5>About Us</h5>
+          <p>
+            For more than 30 years we have been delivering world-class construction and we’ve built many lasting relationships along the way. 
+          </p>
+          <p>
+            We’ve matured into an industry leader and trusted resource for those seeking quality, innovation and reliability when building in the U.S.
+          </p>
+          <a className='btn btn-white'>More on Our History</a>
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
