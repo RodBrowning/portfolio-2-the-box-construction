f8ac0371b4f74aeafcd8946e7e65bfc05d8e8f06
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-05 19:45:37 -0300

    Contact section

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -3,7 +3,35 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className={ContactST.container_fluid}>
+      <div className={`container ${ContactST.container}`}>
+        <h4>What can us do for you?</h4>
+        <h5>
+          We are ready to work on a project of any complexity, whether it’s commercial or residential.
+        </h5>
+        <form>
+          <div>
+            <input type="text" placeholder='Your Name'required />
+          </div>
+          <div>
+            <input type="email" placeholder='Email'required/>
+          </div>
+          <div>
+            <select name="" id="" required>
+              <option value="0">Reason for Contacting</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <input type="phone" placeholder='Phone' />
+          <textarea name="textarea" id="" cols={30} rows={10} placeholder='Message'></textarea>
+          <p><span>*</span> indicates a required field</p>
+          <input type="submit" className='btn btn-primary' value="Submit" />
+        </form>
+      </div>
+    </section>
   )
 }
 
