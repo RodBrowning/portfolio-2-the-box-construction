0ad1667bc39c018ce37cc2646c1ec51bbbd14692
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-04 23:04:33 -0300

    Hero section slider

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,15 +1,15 @@
+import 'swiper/css';
+
 import { ArrowLeft, ArrowRight } from 'akar-icons';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 import HeroST from '../../styles/Hero.module.css'
-import Image from 'next/image'
 import React from 'react'
-import hero from '../../public/assets/Hero.jpg'
 
 const Hero = () => {
   return (
     <section className={HeroST.hero_section}>
       <div className={HeroST.hero_img} style={{backgroundImage: "url(./assets/Hero.jpg)"}}>
-        {/* <Image src={hero} alt="Hero image" /> */}
       </div>
       <div className={`container ${HeroST.container}`}>
         <h1>
@@ -17,19 +17,49 @@ const Hero = () => {
         </h1>
       </div>
       <div className={HeroST.slider}>
-        <div className={HeroST.content}>
-          <h6>Feature Projects</h6>
-          <p>
-            The National University of Architecture
-          </p>
-        </div>
-        <div className={HeroST.slider_nav}>
-          <button className='btn btn-black'><ArrowLeft strokeWidth={2} size={36} /> Back</button>
-          <button className='btn btn-black'>Next <ArrowRight strokeWidth={2} size={36} /></button>
-        </div>
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          navigation>
+          <SwiperSlide>
+            <div className={HeroST.content}>
+              <h6>Feature Projects</h6>
+              <p>
+                The National University of Architecture
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={HeroST.content}>
+              <h6>New Project</h6>
+              <p>
+                The University of Computing Science
+              </p>
+            </div>
+          </SwiperSlide>
+          <div className={HeroST.slider_nav}>
+            <PrevBtn />
+            <NextBtn />
+          </div>
+        </Swiper>
       </div>
     </section>
   )
 }
+
+const PrevBtn = () => {
+  const swiper = useSwiper();
+  return (
+    <button className='btn btn-black' onClick={() => swiper.slidePrev()}><ArrowLeft strokeWidth={2} size={36} /> Back</button>
+  )
+}
+
+const NextBtn = () => {
+  const swiper = useSwiper();
+  return (
+    <button className='btn btn-black' onClick={() => swiper.slideNext()}><ArrowRight strokeWidth={2} size={36} /> Next</button>
+  )
+}
+
 
 export default Hero
