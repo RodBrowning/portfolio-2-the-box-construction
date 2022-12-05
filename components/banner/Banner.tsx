86ab1c392f3dd994065d215aabbd72f962da5f15
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-05 12:27:27 -0300

    Banner section

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -3,7 +3,21 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className={BannerST.container_fluid} style={{backgroundImage: 'url(./assets/banner_image.png)'}}>
+      <div className={`container ${BannerST.container}`}>
+        <div className={BannerST.text_container}>
+          <h4>
+            Free consultation with exceptional quality
+          </h4>
+          <p>
+            Just one call away: <a href="tel:+5511949485112">+55 11 9 4948 5112</a>
+          </p>
+        </div>
+        <div className={BannerST.button_div}>
+          <button className='btn btn-light'>Get your consultation</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
