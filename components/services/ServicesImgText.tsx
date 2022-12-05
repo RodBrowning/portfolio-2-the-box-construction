49d694cf62729ba9e61af4a701ce81303b5e947e
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-05 11:50:32 -0300

    Services Img Text section

--- components/services/ServicesImgText.tsx
+++ components/services/ServicesImgText.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 import ServicesImgTextST from '../../styles/ServicesImgText.module.css'
+import experienceImage from '../../public/assets/experience.png'
 
 const ServicesImgText = () => {
   return (
-    <div>ServicesImgText</div>
+    <section className={`container ${ServicesImgTextST.container}`}>
+      <div className={ServicesImgTextST.image}>
+        <Image src={experienceImage} alt='Experience Image' />
+      </div>
+      <div className={ServicesImgTextST.text_container}>
+        <h2>
+          30 Years Experience
+        </h2>
+        <p>
+          Our company has been the leading provided construction services to clients throughout the USA since 1988.
+        </p>
+        <a className='btn btn-primary' href="#">Contact Us</a>
+      </div>
+    </section>
   )
 }
 
